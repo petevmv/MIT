a74af249f10f73e40dc7cce7cfec31ea7bcc4e45
Maksim Petev <petevmv@gmail.com>
2022-08-15 12:04:08 +0300

    overload the __mul__ operator for V2

--- firstlab/V2/V2.py
+++ firstlab/V2/V2.py
@@ -12,25 +12,31 @@ class V2:
 	def getY(self):
 		return self.y
 
-	def __add__(self, other):
-		x_new_v = self.x + other.x
-		y_new_v = self.y + other.y
-		return V2(x_new_v, y_new_v)
+	def __add__(self, p):
+		return self.add(p)
+		# x_new_v = self.x + other.x
+		# y_new_v = self.y + other.y
+		# return V2(x_new_v, y_new_v)
 
 	def add(self, other):
 		x_new_v = self.x + other.x
 		y_new_v = self.y + other.y
 		return V2(x_new_v, y_new_v)
 
-	def __mul__(self, other):
-		x_new_with_scalar = self.x * scalar
-		y_new_with_scalar = self.y * scalar
-		return(V2(x_new_with_scalar, y_new_with_scalar))
+	# def __mul__(self, other):
+	# 	return self.mul(other)
+	# 	# x_new_with_scalar = self.x * scalar
+	# 	# y_new_with_scalar = self.y * scalar
+	# 	# return(V2(x_new_with_scalar, y_new_with_scalar))
 
-	def mul(self, scalar):
-		x_new_with_scalar = self.x * scalar
-		y_new_with_scalar = self.y * scalar
-		return(V2(x_new_with_scalar, y_new_with_scalar))
+	def mul(self, other):
+		if type(other) == int or type(other) == float:
+			x_new_with_scalar = self.x * other
+			y_new_with_scalar = self.y * other
+			return(V2(x_new_with_scalar, y_new_with_scalar))
+		x_new_with_other_x = self.x * other.x
+		y_new_with_other_y = self.y * other.y
+		return(V2(x_new_with_other_x, y_new_with_other_y))
 
 	def __mul__(self, v):
 		return self.mul(v)
@@ -42,8 +48,11 @@ v1 = V2(1, 2)
 # print(v1.getY())
 v2 = V2(3, 4)
 
-print(v1 + v2)
+# print(v1 + v2)
 print(v1.mul(2))
+print(v1*v2)
+# print(v1)
+# print(v1.add(v2))
 
 # print(v1.add(v2).mul(-1))
 
