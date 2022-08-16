4d21539ae81e861216552d06ab5b68833266f969
Maksim Petev <petevmv@gmail.com>
2022-08-16 16:20:41 +0300

    this commit was intnded to be part of the previous one

--- firstlab/V2/V2.py
+++ firstlab/V2/V2.py
@@ -3,41 +3,40 @@ class V2:
 		self.x = x
 		self.y = y
 
+
 	def __str__(self):
 		return f'({self.x}, {self.y})'
+
 
 	def getX(self):
 		return self.x
 
+
 	def getY(self):
 		return self.y
 
+
 	def __add__(self, p):
 		return self.add(p)
-		# x_new_v = self.x + other.x
-		# y_new_v = self.y + other.y
-		# return V2(x_new_v, y_new_v)
+		
 
 	def add(self, other):
 		x_new_v = self.x + other.x
 		y_new_v = self.y + other.y
 		return V2(x_new_v, y_new_v)
 
-	# def __mul__(self, other):
-	# 	return self.mul(other)
-	# 	# x_new_with_scalar = self.x * scalar
-	# 	# y_new_with_scalar = self.y * scalar
-	# 	# return(V2(x_new_with_scalar, y_new_with_scalar))
 
 	def mul(self, other):
 		if type(other) == int or type(other) == float:
 			x_new_with_scalar = self.x * other
 			y_new_with_scalar = self.y * other
 			return(V2(x_new_with_scalar, y_new_with_scalar))
+		
 		x_new_with_other_x = self.x * other.x
 		y_new_with_other_y = self.y * other.y
 		return(V2(x_new_with_other_x, y_new_with_other_y))
 
+	
 	def __mul__(self, v):
 		return self.mul(v)
 			
