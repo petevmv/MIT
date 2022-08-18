a4ef4f34014d7b964568109499889e97c5662df9
Maksim Petev <petevmv@gmail.com>
2022-08-19 02:32:05 +0300

    overload the __mul__ method, removed some print statements and pseudo code

--- firstlab/polynomial/polynomial.py
+++ firstlab/polynomial/polynomial.py
@@ -58,15 +58,13 @@ class Polynomial:
 		p1 = self.coefficients
 		p2 = other.coefficients
 
-		# if len(self.coefficients) < len(other.coefficients):
-		# 	p1 = [1] * (len(other.coefficients) - len(self.coefficients)) + p1
-		# if len(self.coefficients) > len(other.coefficients):
-		# 	p2 = [1] * (len(self.coefficients) - len(other.coefficients)) + p2
-		print(p1,self.degree, p2, other.degree)
 		order_p1 = self.degree
 		order_p2 = other.degree
+		# print(p1,order_p1, p2, order_p2)
+		
 		tup_p1 = []
 		tup_p2 = []
+		
 		for i in p1:
 			tup_p1.append((i, order_p1))
 			order_p1 -= 1
@@ -77,7 +75,7 @@ class Polynomial:
 		for x in tup_p1:
 			for y in tup_p2:
 				tup_of_coeff_and_power.append((x[0] * y[0], x[1] + y[1]))
-		print(tup_of_coeff_and_power)
+		# print(tup_of_coeff_and_power)
 
 		# This block of code will add all coeffs of the same order
 		d = defaultdict(float)
@@ -85,13 +83,16 @@ class Polynomial:
 		for x, y in tup_of_coeff_and_power:
 			d[y] += float(x)
 
-		# returning to list structure to pass it to polinomial class
+		# returning to list structure to pass it to polynomial class
 		result = []
 		for x in d.items():
 			result.append(x[1])
 
 		return Polynomial(result)
 
+
+	def __mul__(self, p):
+		return self.mul(p)
 
 	def __str__(self):
 		equation = ''
@@ -122,11 +123,6 @@ class Polynomial:
 
 p1 = Polynomial([1,2,3])
 p2 = Polynomial([100, 200])
-# x**2 + 2x + 3
-# 100x + 200
-
-# 100x**3 + 200x**2 + 200x**2 + 400x + 300x + 600
-# 100x**3 + 400x**2 + 700x + 600
 
 
 # print(p1.add(p2))
@@ -134,9 +130,4 @@ p2 = Polynomial([100, 200])
 # print(p1(-1))
 # print(p1.mul(2))
 print(p1.mul(p2))
-
-
-# print(p1)
-# print(p2)
-# print(p3)
-# print(p4)
+print(p1 * p2)
