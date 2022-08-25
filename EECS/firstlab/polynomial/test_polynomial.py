e434289e83fa15d6b7886c325e480aff723e2b21
Maksim Petev <petevmv@gmail.com>
2022-08-25 17:22:11 +0300

    removed missplaced comma

--- EECS/firstlab/polynomial/test_polynomial.py
+++ EECS/firstlab/polynomial/test_polynomial.py
@@ -4,7 +4,7 @@ from polynomial import Polynomial
 
 class TestPolynomial(unittest.TestCase):
     def test_build_visual_of_equasion(self):
-        tests_for_visual_of_eq = [(0, "",), (1, "z"), (2, "z**2",), (3, "z**3")]
+        tests_for_visual_of_eq = [(0, ""), (1, "z"), (2, "z**2"), (3, "z**3")]
         for element, expected in tests_for_visual_of_eq:
             with self.subTest(f"{element} was given, expected result {expected}"):
                 self.assertEqual(Polynomial.build_visual_of_equation(element), expected)
