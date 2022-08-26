de4a10e6a1fdf4123b65ec1faa7adaa8ca2df76b
Maksim Petev <petevmv@gmail.com>
2022-08-26 11:07:47 +0300

    added test for .val method

--- EECS/firstlab/polynomial/test_polynomial.py
+++ EECS/firstlab/polynomial/test_polynomial.py
@@ -9,6 +9,17 @@ class TestPolynomial(unittest.TestCase):
             with self.subTest(f"{element} was given, expected result {expected}"):
                 self.assertEqual(Polynomial.build_visual_of_equation(element), expected)
 
+    def test_val(self):
+        tests_equation_val_expected = [
+            ([1, 2, 3], 2, 11),
+            ([2, 3, 4], 3, 31),
+            ([2, 4], 5, 14),
+            ([1, 2, 3, 4], 3, 58),
+        ]
+        for polynom, value, expected in tests_equation_val_expected:
+            with self.subTest(f"{Polynomial(polynom)} / z={value} ==> {expected}"):
+                self.assertEqual(Polynomial(polynom).val(value), expected)
+
 
 if __name__ == "__main__":
     unittest.main()
